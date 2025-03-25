ccda62a3495dcee892d43f2dca63065c5eff33df
Geonho0927 <moneljjang1234@naver.com>
2025-03-25 19:35:03 +0900

    초기세팅

---
+++ src/main/kotlin/study/study/StudyApplication.kt
@@ -0,0 +1,7 @@
+package study.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyApplication
